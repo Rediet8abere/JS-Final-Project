1acb10263dff66489e77bd6ae06ef118826a5490
Rediet8abere <redietaberegesesse@gmail.com>
2021-03-02 14:22:53 -0800

    memeproject

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
-// const fetch = require("node-fetch");
-// import '../node_modules/cross-fetch/polyfill';
+import config from '../config.js';
 
-// console.log("hello world")
-async function getMeme(memeType, Top, BottomText) {
-  const path = `http://apimeme.com/meme?meme=${memeType}&top=${Top}&bottom=${BottomText}`
-  // http://apimeme.com/meme?meme=10-Guy&top=Top+text&bottom=Bottom+text
+async function getMeme(query) {
+  const path = `https://api.tenor.com/v1/search?q=${query}&key=${config.SECRET_API_KEY}&limit=12`
   try {
-    const res = await fetch(path, {mode:"no-cors"})
-    console.log("getMeme--->", res)
+    const res = await fetch(path)
     const json = await res.text()
-    console.log("json--->", json)
     return json
   } catch(err) {
     console.log("Error", err)
     return err
   }
-
-  
-  
 }
 export default getMeme
-
-// getMeme('10-Guy', 'Top+text', 'Bottom+text')
